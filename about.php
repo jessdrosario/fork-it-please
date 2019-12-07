d48683750b319a3599ed367a609e85c1fcc31103
jessdrosario <jdelrosario97@gmail.com>
2019-12-07 14:41:56 +0800

    added baby photo and about section

--- about.php
+++ about.php
@@ -10,7 +10,25 @@
   ?>
 
   <!-- Page Content -->
-  <div class="container"></div>
+  <div class="container">
+  	<div class="about-us-section" style="display: flex; flex-direction: row; justify-content: center;">
+  		<div class="manuel">
+  			<img src="./images/baby.jpg" height="250px" width="250px">
+  			<h1>Ako si Manuel</h1>
+  			<h6>I'm manuel basura man</h6>
+  		</div>
+  		<div class="Jess">
+  			<img src="./images/baby.jpg" height="250px" width="250px">
+  			<h1>Ako si Superman</h1>
+  			<h6>Hoping to be the best dev Evaahh</h6>
+  		</div>
+  		<div class="Michael">
+  			<img src="./images/baby.jpg" height="250px" width="250px">
+  			<h1>Hi I'm Michael!</h1>
+  			<h6>Let's plaaaaay!!~~</h6>
+  		</div>
+  	</div>
+  </div>
   <!-- /.container -->
 
  <?php include_once("./components/footer.php"); ?>
