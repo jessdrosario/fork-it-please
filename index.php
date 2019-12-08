cf156ae314b802dd0aeefaa634ac04406385b93c
unknown <delanrecca@gmail.com>
2019-12-07 16:52:11 -0800

    donation tag

--- index.php
+++ index.php
@@ -2,17 +2,49 @@
 <html lang="en">
 
   <?php include_once("./components/header.php"); ?>
+<head>
+  
 
+
+</head>
+
+<style>
+  img {
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      padding: 5px;
+
+
+  }
+  img:hover {
+    box-shadow: 0 0 2px 1px rgba(0,140,186, 0.5);
+  }
+
+</style>
 <body>
 
   <?php 
     include_once("./components/menu.php");
   ?>
 
-  <!-- Page Content -->
-  <div class="container">
-    <img src="./images/manuel.jpg" style="height:500px; width: 500px; ">
-  </div>
+    <!-- Page Content -->
+  <div style="padding-bottom: 15px;">  
+    <div class="container " style="display: flex; justify-content: center; ">
+      <div style="justify-content: center;text-align: center;align-content: center;">
+        <img class="col-sm-12" src="./images/manuel.jpg" style="height: 40rem; width: 40rem"; >
+        <h1 class="col-sm-12 col-md-6 col-lg-12" style="font-size: 100px; "> NEW SPECIES</h1>
+        <h2> Please Help !</h2>
+
+      </div>
+      
+    </div>
+
+
+    <div style="display: flex; justify-content: center; flex-direction: column; text-align: center;align-content: center;">
+        <h1><a href="about.php">Donation</a></h1>
+        <input type="Number" name="Donation" placeholder="lowest 10k" style="max-width: 150px;">
+    </div>
+  </div>  
   <!-- /.container -->
 
  <?php include_once("./components/footer.php"); ?>
